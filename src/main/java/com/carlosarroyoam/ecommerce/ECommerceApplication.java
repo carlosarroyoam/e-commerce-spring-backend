79d345e47beea58ae5d1208422a6ac3ce869f179
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2026-08-23 15:48:02 -0600

    Agrega javadocs a método main

--- src/main/java/com/carlosarroyoam/ecommerce/ECommerceApplication.java
+++ src/main/java/com/carlosarroyoam/ecommerce/ECommerceApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /** Punto de entrada de la aplicación Spring Boot del API de e-commerce. */
 @SpringBootApplication
 public class ECommerceApplication {
+  /**
+   * Arranca el contexto de Spring Boot.
+   *
+   * @param args argumentos de línea de comandos
+   */
   public static void main(String[] args) {
     SpringApplication.run(ECommerceApplication.class, args);
   }
